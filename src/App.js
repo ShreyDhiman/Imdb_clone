fcb0a34b9844b2a6b6a220ed6f8510885de7231c
Shrey Dhiman <sdhiman@aapnainfotech.com>
2022-02-09 15:20:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Movie from "./movie";
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    name: {},
+  };
+
+  onSearchSubmit = async (e) => {
+    const response = await axios.get(
+      `https://www.omdbapi.com/?s=${e}&apikey=21193e36`
+    );
+    // console.log(response.data);
+
+    response.data.Search.forEach(async (elemnet) => {
+      const Response = await axios.get(
+        `https://www.omdbapi.com/?i=${elemnet.imdbID}&apikey=21193e36`
+      );
+      console.log(Response.data);
+    });
+  };
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "30px" }}>
+        <Movie onSearchSubmit={this.onSearchSubmit} />
+        {/* <Response temp={this.state.temp} /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
